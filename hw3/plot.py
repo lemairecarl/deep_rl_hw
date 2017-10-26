2b0844a1ccab0cd2b5ccad01eb5706d73db89581
Carl Lemaire <c.lemaire@live.ca>
2017-10-26 16:11:44 -0400

    Remove bad rows

--- hw3/plot.py
+++ hw3/plot.py
@@ -9,11 +9,17 @@ args = parser.parse_args()
 with open(args.results_file, 'r') as f:
     steps = list(f.readlines())
 
+# There are 3 columns: Timesteps (7 char), [space], Mean reward (8 char), [space], Best mean reward (8 char).
 to_tuple = lambda s: (int(s[0:7]), float(s[8:16]), float(s[17:25]))
 
 tsteps = [to_tuple(s) for s in steps]
 
-t, mean_r, best_r = [x[0] for x in tsteps], [x[1] for x in tsteps], [x[2] for x in tsteps]
+# The 3rd column, "Best mean reward", is the best mean reward over 100 episodes. If there are not yet 100 episodes seen,
+# the value given is -infinity. Let's remove those rows.
+tsteps = [x for x in tsteps if x[2] != float('-inf')]
+
+t, mean_r, best_r = zip(*tsteps)  # zip(*x) can be seen as unzip(x), going from list(tuples) to tuple(lists).
+# In fact, zip() returns a list, but you can assign it to a tuple, like I did.
 
 
 def plot_dict(x, d):
